039eeb87ffdec62262aef83b8d014e07db7f07e7
Vitalii Panchenko <panchenko.vitaliy@gmail.com>
2026-09-03 18:13:02 +0300

    suppliers: migration, model, factory

---
+++ database/seeders/SuppliersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Supplier;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SuppliersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->getItems() as $item) {
+            if ($this->exists($item)) {
+                continue;
+            }
+
+            Supplier::factory()->create($item);
+        }
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    protected function exists($item)
+    {
+        return Supplier::query()->where('external_id', data_get($item, 'external_id'))
+            ->exists();
+    }
+
+    /**
+     * @return array
+     */
+    protected function getItems()
+    {
+        return [
+            [
+                'name' => 'Test Supplier',
+                'external_id' => 'test-supplier',
+            ]
+        ];
+    }
+}
